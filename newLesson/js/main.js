fee408bdad9d1a9be1c74b6d6395e12fcaec6b05
Bashir Azizov <89950169+bashirazizov@users.noreply.github.com>
2022-01-08 15:06:07 +0400

    Add files via upload

---
+++ newLesson/js/main.js
@@ -0,0 +1,70 @@
+$(document).ready(function () {
+  // $(".owl-carousel").owlCarousel({
+  //   loop:true,
+  //   margin: 10,
+  //   nav: true,
+  //   autoplay:true,
+  //   autoplayTimeout:1000,
+  //   autoplayHoverPause:true,
+  //   animateOut: 'fadeOut',
+  //   stagePadding:80,
+  //   responsive:{
+  //       0:{
+  //           items:1
+  //       },
+  //       600:{
+  //           items:2
+  //       },
+  //       1000:{
+  //           items:3
+  //       }
+  //   }
+  // });
+  //   $(".owl-carousel").owlCarousel({
+  //     animateOut: 'flipOutX',
+  //     animateIn: 'flipInX',
+  //     nav:true,
+  //     loop: true,
+  //     autoplay: true,
+  //     autoplayTimeout: 2000,
+  //     autoplayHoverPause: true,
+  //     animateOut: "fadeOut",
+  //     items: 1,
+  //   });
+  //   $(".banner-carousel").owlCarousel({
+  //     loop: true,
+  //     autoplay: true,
+  //     autoplayTimeout: 3000,
+  //     autoplayHoverPause: false,
+  //     items: 1,
+  //   });
+  //   $(".items-carousel").owlCarousel({
+  //     loop:true,
+  //     margin: 10,
+  //     nav: true,
+  //     autoplay:true,
+  //     autoplayTimeout:1000,
+  //     autoplayHoverPause:true,
+  //     animateOut: 'fadeOut',
+  //     stagePadding:80,
+  //     responsive:{
+  //         0:{
+  //             items:1
+  //         },
+  //         600:{
+  //             items:2
+  //         },
+  //         1000:{
+  //             items:3
+  //         }
+  //     }
+  //   });
+
+  //   AOS.init();
+
+  //   $(document).on("click", ".hamburger", function () {
+  //       $(this).toggleClass("is-active")
+  //   });
+
+  $("#studentsTable").DataTable();
+});
